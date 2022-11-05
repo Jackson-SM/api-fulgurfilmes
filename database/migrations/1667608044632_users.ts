9d6cb2f476e0e2b688ecb2af3ab860974e5c79fa
Jackson da Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2022-11-04 23:08:05 -0300

    updated: migration user

--- database/migrations/1667608044632_users.ts
+++ database/migrations/1667608044632_users.ts
@@ -3,10 +3,13 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
+      table.increments('id').primary().unique()
+      table.string('email').notNullable().unique()
+      table.string('name').notNullable()
+      table.string('password').notNullable()
+      table.integer('level').defaultTo(1).notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
@@ -15,7 +18,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
