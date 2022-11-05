86795becb3a491ce59f022173676c32d2cbc182a
Jackson da Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2022-11-04 23:22:25 -0300

    Added: functions controller generated

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,13 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class UsersController {}
+export default class UsersController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
