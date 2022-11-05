e610e3af0aa809321800af426cdbb19f8e1ae676
Jackson da Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2022-11-04 23:48:59 -0300

    Added: group route created Updated: Implements function of controller in UsersController Insert, Read, Update and Delete

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,13 +1,43 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const users = await User.all()
 
-  public async store({}: HttpContextContract) {}
+    return users
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const { email, name, password } = request.only(['email', 'name', 'password'])
 
-  public async update({}: HttpContextContract) {}
+    const user = await User.create({
+      email,
+      name,
+      password,
+    })
 
-  public async destroy({}: HttpContextContract) {}
+    return user
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+
+    return user
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+    const { name, password } = request.only(['name', 'password'])
+
+    user.merge({ name, password })
+
+    await user.save()
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+
+    await user.delete()
+  }
 }
